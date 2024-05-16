7cfcd45c4e68bef0fee5f8c30cc199a53f6997b7
Laurent Itti <jevois.org@gmail.com>
2024-05-15 17:30:50 -0700

    upgrading to Ubuntu 24.04 noble

--- facedetector/rebuild-pro-host.sh
+++ facedetector/rebuild-pro-host.sh
@@ -5,9 +5,6 @@ set -e
 # Get dlib if not here:
 if [ ! -d dlib ]; then ./download-dlib.sh; fi
 
-# Get the external contributed packages if they are not here or are outdated:
-./Contrib/check.sh
-
 sudo /bin/rm -rf phbuild \
     && mkdir phbuild \
     && cd phbuild\
