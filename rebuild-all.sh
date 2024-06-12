0fd68162a9705e1803fc37f6b2beeb6705042600
Laurent Itti <jevois.org@gmail.com>
2024-06-12 15:38:03 -0700

    build script update

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+# usage: rebuild-all.sh [a33|pro|all]
+
+if [ "X$1" = "Xa33" ]; then build_a33=1
+elif [ "X$1" = "Xpro" ]; then build_pro=1
+elif [ "X$1" = "Xall" ]; then build_a33=1; build_pro=1;
+else echo "usage: rebuild-all.sh [a33|pro|all]"; exit 2;
+fi
 
 for d in `find . -type d`; do
     if [ -f "${d}/rebuild-host.sh" ]; then
         echo "#################### Rebuilding ${d} ####################"
         cd "$d"
-        ./rebuild-host.sh
-        ./rebuild-platform.sh --staging
-        ./rebuild-pro-host.sh
-        ./rebuild-pro-platform-pdeb.sh
+        if [ "X$build_a33" = "X1" ]; then
+            ./rebuild-host.sh
+            ./rebuild-platform.sh --staging
+        fi
+        
+        if [ "X$build_pro" = "X1" ]; then
+            ./rebuild-pro-host.sh
+            ./rebuild-pro-platform-pdeb.sh
+        fi
         cd ..
     fi
 done
